78941c069660a12eac9d152e649327e5c20e3bef
Angelique <angelicrf@gmail.com>
2022-06-20 09:54:38 -0700

    v2 boatone alarmsystem

--- BoatRunners/Assets/Scripts/BoatMenu.cs
+++ BoatRunners/Assets/Scripts/BoatMenu.cs
@@ -1,10 +1,29 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BoatMenu : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject displayMenu;
     public GameObject gpsMap;
+    public GameObject alarm;
+    public Button alarmOn;
+    public Button alarmOff;
+    private bool colorArm = false;
+    private bool colorDisarm = false;
+    private void FixedUpdate()
+    {
+        if (colorDisarm)
+        {
+            alarmOn.GetComponent<Image>().color = alarmOn.colors.disabledColor;
+            alarmOff.GetComponent<Image>().color = alarmOff.colors.selectedColor;
+        }
+        if (colorArm)
+        {
+            alarmOff.GetComponent<Image>().color = alarmOff.colors.disabledColor;
+            alarmOn.GetComponent<Image>().color = alarmOn.colors.selectedColor;
+        }
+    }
     public void StartBoatSystem()
     {
         mainMenu.SetActive( false );
@@ -15,5 +34,23 @@ public class BoatMenu : MonoBehaviour
         displayMenu.SetActive( false );
         gpsMap.SetActive( true );
     }
-
+    public void BoatAlarmSystem()
+    {
+        displayMenu.SetActive( false );
+        alarm.SetActive( true );
+    }
+    public void AlarmArmSystemOn()
+    {
+        //alarmOff.enabled = false;
+        //alarmOn.enabled = true;
+        colorArm = true;
+        colorDisarm = false;
+    }
+    public void AlarmArmSystemOff()
+    {
+        //alarmOn.enabled = false;
+        //alarmOff.enabled = true;
+        colorDisarm = true;
+        colorArm = false;
+    }
 }
--- BoatRunners/Assets/Scripts/BoatSeatsOne.cs
+++ BoatRunners/Assets/Scripts/BoatSeatsOne.cs
@@ -6,23 +6,11 @@ public class BoatSeatsOne : MonoBehaviour
     public GameObject hand;
     public GameObject player;
     public GameObject parentObj;
-    private bool isChangedPos = false;
-    private Vector3 seatPos;
-    private Quaternion seatRot;
-
-    private void Start()
-    {
-        seatRot = Quaternion.Euler( player.transform.rotation.x, player.transform.rotation.y + 180f , player.transform.rotation.z);
-    }
     private void FixedUpdate()
     {
         if (BoatOneStatics.isSat)
         {
-            seatPos = new Vector3( gameObject.transform.position.x + 0.5f , gameObject.transform.position.y - 0.6f , transform.position.z - 1f );
-            player.transform.position = seatPos;
-            player.transform.rotation = seatRot;
-            //player.isStatic = true;
-            //player.transform.parent = parentObj.transform;
+            player.transform.parent = parentObj.transform;
         }
     }
     private void OnTriggerEnter(Collider other)
