187fdd937dde2aca03c1090adff336e9fdcd69df
Your Name <you@example.com>
2022-07-01 16:31:08 -0800

    v4 boatOne driver-anim speech

--- BoatRunners/Assets/Scripts/DriverAnimController.cs
+++ BoatRunners/Assets/Scripts/DriverAnimController.cs
@@ -2,16 +2,26 @@ using UnityEngine;
 
 public class DriverAnimController : MonoBehaviour
 {
-    private void Awake()
+    public AudioClip greetingDialog;
+    void FixedUpdate()
     {
-        gameObject.GetComponent<Animator>().SetBool("isSitidle", true);
-    }
-    //void FixedUpdate()
-    //{
+        //Hey, Welcome to the show, Luckily the weather is on our side today, how may I help you?
+        if (BoatOneStatics.isBoatTeleported)
+        {
+            if (!gameObject.GetComponent<AudioSource>().enabled)
+            {
+                gameObject.GetComponent<AudioSource>().enabled = true;
+                if (gameObject.GetComponent<AudioSource>().enabled)
+                {
+                    gameObject.GetComponent<AudioSource>().Play();
+                    gameObject.GetComponent<AudioSource>().volume = 1f;
+                }
+            }
+        }
 
-    //    if (BoatOneStatics.isSpeechDone)
-    //    {
-    //        other funstionality
-    //    }
-    //}
+        //if (BoatOneStatics.isSpeechDone)
+        //{
+          
+        //}
+    }
 }
---
+++ BoatRunners/Assets/Scripts/HandCell.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using UnityEngine;
+
+public class HandCell : MonoBehaviour
+{
+    
+    void Update()
+    {
+        
+    }
+    private IEnumerator CellMove()
+    {
+        yield return new WaitForSeconds(5f);
+        //transform.position = 
+    }
+}
