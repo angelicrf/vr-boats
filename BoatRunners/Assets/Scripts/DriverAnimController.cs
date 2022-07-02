32dd72fb377d1d87b5c0c944213ca05b91517522
Your Name <you@example.com>
2022-07-01 19:18:36 -0800

    v4 boatOne driver-anim speech2

--- BoatRunners/Assets/Scripts/DriverAnimController.cs
+++ BoatRunners/Assets/Scripts/DriverAnimController.cs
@@ -2,26 +2,65 @@ using UnityEngine;
 
 public class DriverAnimController : MonoBehaviour
 {
+    private bool isResponded = false;
+    private bool isAudioChanged = false;
     public AudioClip greetingDialog;
+    
     void FixedUpdate()
     {
-        //Hey, Welcome to the show, Luckily the weather is on our side today, how may I help you?
         if (BoatOneStatics.isBoatTeleported)
         {
-            if (!gameObject.GetComponent<AudioSource>().enabled)
+            if (!gameObject.GetComponent<AudioSource>().enabled && !gameObject.GetComponent<Animator>().enabled)
             {
+                gameObject.GetComponent<Animator>().enabled = true;
                 gameObject.GetComponent<AudioSource>().enabled = true;
-                if (gameObject.GetComponent<AudioSource>().enabled)
+                if (gameObject.GetComponent<AudioSource>().enabled && gameObject.GetComponent<Animator>().enabled)
                 {
+                    //gameObject.GetComponent<Animator>().StartPlayback();
                     gameObject.GetComponent<AudioSource>().Play();
                     gameObject.GetComponent<AudioSource>().volume = 1f;
                 }
             }
         }
-
-        //if (BoatOneStatics.isSpeechDone)
+        //else
         //{
-          
+        //    if (gameObject.GetComponent<AudioSource>().enabled)
+        //    {
+        //        gameObject.GetComponent<AudioSource>().enabled = false;
+        //    }
         //}
+        if (gameObject.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Stand-TalkingCell"))
+        {
+            if (gameObject.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !gameObject.GetComponent<Animator>().IsInTransition(0))
+            {
+                gameObject.GetComponent<Animator>().SetBool("isTalking", true);
+            }
+        }
+        if (!isAudioChanged)
+        {
+            if (gameObject.GetComponent<Animator>().GetBool("isTalking"))
+            {
+            
+                if (greetingDialog)
+                {
+                    gameObject.GetComponent<AudioSource>().clip = greetingDialog;
+                    gameObject.GetComponent<AudioSource>().Play();
+                    gameObject.GetComponent<AudioSource>().volume = 1f;
+                    isAudioChanged = true;
+                    isResponded = true;
+                }
+            }
+        }
+        if (BoatOneStatics.isSpeechDone)
+        {
+            if (isResponded)
+            {
+                if (BoatOneStatics.speechText.Split(' ')[0] == "yes" || BoatOneStatics.speechText.Split(' ')[0] == "ya")
+                {
+                    // run a driving animation
+                    Debug.Log("letsDrive...");
+                }
+            }
+        }
     }
 }
