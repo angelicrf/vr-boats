5c6fed2a76b7db6fcddba29f048bee8ff576b656
Your Name <you@example.com>
2022-07-10 12:06:45 -0800

    v4 boatOne driver anim-pos fixed

--- BoatRunners/Assets/Scripts/DriverDriveAnim.cs
+++ BoatRunners/Assets/Scripts/DriverDriveAnim.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class DriverDriveAnim : MonoBehaviour
@@ -6,6 +7,7 @@ public class DriverDriveAnim : MonoBehaviour
     public GameObject thisSeat;
     public GameObject boatMenu;
     private bool isInputDevices = false;
+    private bool isEntered = false;
 
     private void FixedUpdate()
     {
@@ -13,9 +15,16 @@ public class DriverDriveAnim : MonoBehaviour
             
         {
             gameObject.transform.localRotation = Quaternion.Euler(0,0,0);
-            gameObject.transform.localPosition = new Vector3( thisSeat.transform.localPosition.x -0.1f,-1.25f,thisSeat.transform.localPosition.z + 0.65f);
+            
             if (!BoatOneStatics.isBoatOneStartDrive)
             {
+                if (gameObject.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Start"))
+                {
+                    if (!isEntered)
+                    {
+                        StartCoroutine(MakeTimeChanges());
+                    }
+                }
                 StartCoroutine(DriverStatics.MoveToDriveAnimCo(gameObject, "isDriving", res => BoatOneStatics.isDriveBoatOne = res));
             }
             if (BoatOneStatics.isBoatOneStartDrive && !BoatOneStatics.isDriveBoatOne)
@@ -35,5 +44,12 @@ public class DriverDriveAnim : MonoBehaviour
         //{
         //    
         //}
+    }
+    private IEnumerator MakeTimeChanges()
+    {
+        gameObject.transform.localPosition = new Vector3(thisSeat.transform.localPosition.x - 0.1f, 0, thisSeat.transform.localPosition.z + 0.65f);
+        yield return new WaitForSeconds(2f);
+        gameObject.transform.localPosition = new Vector3(thisSeat.transform.localPosition.x - 0.1f, -1.24f, thisSeat.transform.localPosition.z + 0.65f);
+        isEntered = true;
     }
 }
