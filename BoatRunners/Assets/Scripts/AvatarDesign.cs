7878ffa218cca48f0861975bd452d7c24c41cbde
Your Name <you@example.com>
2022-07-11 15:31:40 -0800

    v5 avatar design scene added

---
+++ BoatRunners/Assets/Scripts/AvatarDesign.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class AvatarDesign : MonoBehaviour
+{
+   public void ClothesIcon()
+    {
+        Debug.Log("btnCloth..");
+    }
+    public void AccessoriesBtn()
+    {
+        Debug.Log("btnAccessories..");
+    }
+    public void ButtomBtn()
+    {
+        Debug.Log("btnButtom..");
+    }
+    public void TopBtn()
+    {
+        Debug.Log("btnTop..");
+    }
+    public void ShoesBtn()
+    {
+        Debug.Log("btnShoes..");
+    }
+  
+}
